6e6f1809ca2c55e5417f1fde7ae0f4e0b8039685
Harry Ho <hemil0102@gmail.com>
2026-03-13 23:29:44 +0900

    init 초기 프로젝트 파일 업로드

---
+++ Font Practice/Font Practice/Font_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Font_PracticeApp.swift
+//  Font Practice
+//
+//  Created by harryho on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Font_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
