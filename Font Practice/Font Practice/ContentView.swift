7536a0f83c4fa4e19fa26aea34ea4d1e78584563
Harry Ho <hemil0102@gmail.com>
2026-03-14 12:26:05 +0900

    feature: .font()를 통한 sementic font 가능 종류 확인

--- Font Practice/Font Practice/ContentView.swift
+++ Font Practice/Font Practice/ContentView.swift
@@ -9,12 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        VStack(alignment: .leading) {
+            Text(".font(.largeTitle)")
+                .foregroundStyle(.gray)
             Text("Hello, world!")
+                .font(.largeTitle)
+            
+            Text(".font(.title)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.title)
+            
+            Text(".font(.title2)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.title2)
+            
+            Text(".font(.title3)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.title3)
+            
+            Text(".font(.headline)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.headline)
+            
+            Text(".font(.subheadline)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.subheadline)
+            
+            Text(".font(.body)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.body)
+            
+            Text(".font(.callout)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.callout)
+            
+            Text(".font(.caption)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.caption)
+            
+            Text(".font(.caption2)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.caption2)
+            
+            Text(".font(.footnote)")
+                .foregroundStyle(.gray)
+            Text("Hello, world!")
+                .font(.footnote)
         }
+        .frame(maxWidth: .infinity, alignment: .leading)
         .padding()
     }
 }
