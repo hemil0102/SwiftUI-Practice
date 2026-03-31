6a6456b2f0d38a2ee60806fb577adb24c894f0bb
Harry Ho <hemil0102@gmail.com>
2026-03-31 20:31:09 +0900

    init. List 연습 프로젝트 생성

---
+++ List Practice/List Practice/List_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_PracticeApp.swift
+//  List Practice
+//
+//  Created by harryho on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct List_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
